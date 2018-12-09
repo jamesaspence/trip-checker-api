7be80646f862280bec9305a9672244c444104bca
James Spence <Jamesaspence20@gmail.com>
2018-12-08 23:23:42 -0600

    new auth tokens table

---
+++ database/migrations/2018_12_09_051612_create-auth_tokens-table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auth_tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('token');
+            $table->timestamps();
+        });
+
+        Schema::table('auth_tokens', function (Blueprint $table) {
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auth_tokens');
+    }
+}
